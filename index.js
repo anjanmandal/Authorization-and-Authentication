ea515a2a209fae35135f1fe23bda01ed6335ca4c
Anjan Mandal <anjanmandal2076@gmail.com>
2024-05-23 02:57:27 -0500

    added more features

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express= require("express");
 const app=express();
+const config =require('dotenv')
+config.config();
 const mongoose=require('mongoose');
 const path=require('path');
 const User=require('./schema/schema.js');
+const customError=require("./ErrorHandler/ExpressError.js");
+const jwt=require('jsonwebtoken');
+const util=require('util');
+const sendEmail=require("./nodemailer  ");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('./public'));//serving static file
 
+const signToken=(id)=>{
+    return jwt.sign({id},process.env.SECRET_STR,{expiresIn:'1h'});
+}
 
 main().then(()=>{
     console.log("Connected to DB..")
@@ -30,6 +39,65 @@ app.get("/signup",(req,res)=>{
     console.log("got it")
     res.render("signUp.ejs");
 })
+
+const midwareToProtectInfo = async (req, res, next) => {
+    // Read the token from the authorization header
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    let token;
+    
+    if (authHeader && authHeader.toLowerCase().startsWith('bearer')) {
+        token = authHeader.split(' ')[1];
+    }
+
+    // If no token is provided, return an error
+    if (!token) {
+        return next(new customError(400, "Please log in!"));
+    }
+
+    // Validate the token
+    try {
+        const decodedToken = await util.promisify(jwt.verify)(token, process.env.SECRET_STR);
+        console.log(decodedToken);
+        const user=await User.findById(decodedToken.id);// Store decoded token in the request object
+        next(); // Proceed to the next middleware or route handler
+    } catch (err) {
+        return next(new customError(400, "Token validation failed!"));
+    }
+
+    //chek if the user exists or not because sometimes even if the user is logged in server might delete its accoutn..
+    if(!user){
+        next(new customError(401,"User doesnot exists"))
+    }
+
+    //if the user change his passoword when token was issued..
+    const isPasswordChanged=await user.isChangedPassword(decodedToken.iat);
+    if(isPasswordChanged){
+        next(new customError(401,"Your password was changed recently please login again.."))
+    }
+    req.user=user;
+
+}
+app.get("/forgot-password",(req,res)=>{
+    res.render("changePass.ejs");
+})
+app.post("/change-password",async (req,res)=>{
+    //three things need to be done here
+    //1. Check if the user exists or not it not throw err
+    //2. Generate a random reset token, this token doesnot need to be that strong so we will import one library "crypto". const crypto= require("crypto")
+    //3. Send the token back to the user email
+    //1
+    let {email}=req.body;
+    const user=await User.findOne({email});
+    if(!user){
+        next(new customError(404,"couldnot find the user with given email."))
+    }
+    const resetToken=user.createResetPasswordToken();
+    await user.save({validateBeforeSave:false});
+   await sendEmail();
+
+})
+
 app.post("/signup",async(req,res)=>{
     let{name,email,password,confirmPassword}=req.body;
     let user1= new User({
@@ -39,7 +107,45 @@ app.post("/signup",async(req,res)=>{
         confirmPassword:confirmPassword
 
     });
-    user1.save().then((res)=>{
-        console.log(res);
-    })
+    try {
+        await user1.save();
+        console.log('User saved successfully.');
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            // Handle validation error
+            for (let field in err.errors) {
+                console.error(`Validation error: ${err.errors[field].message}`);
+            }
+        } else {
+            // Handle other errors
+            console.error('An error occurred:', err);
+        }
+    }
+})
+app.get("/login",(req,res)=>{
+    res.render("login.ejs");
+})
+app.post("/login",async(req,res,next)=>{
+    let{email,password}=req.body;
+    console.log(email)
+    if(!email || !password){
+   return next(new customError(400,"invalid email or password"));
+    }
+    let user=await User.findOne({email});
+   if(!user || !(await user.comparePassword(password,user.password))){
+    return next(new customError(400,"Incorrect email or passoword")) ;
+   }
+   const token=signToken(user._id);
+   console.log(token);
+   res.render("alluser.ejs",{token})
+});
+app.get("/alluser",midwareToProtectInfo,(req,res)=>{
+    console.log(req.header.authorization)
+
+})
+app.use("/login",(err,req,res,next)=>{
+    console.log("error occured");
+    let{status=500 ,message="some error occured"}=err;
+    res.status(status).send(message);
+
 })
--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt=require('bcryptjs')
 const validator=require("validator");
+const crypto=require('crypto');
 
 const Userschema = new mongoose.Schema({
     name:{
@@ -24,9 +25,52 @@ const Userschema = new mongoose.Schema({
         type:String,
         required:[true,'please confirm your password'],
         //Note: You need to make sure that the both password and confirm password are same
-    }
+        validate:{//tbis will only work for save and create...
+            validator:function(val){
+                return val===this.password;
+            },
+            message:'password and confirm password doesnot match..'
+        }
+    },
+    passwordChangedAt:Date,
+    passwordResetToken:String,
+    passwordResetTokenExpires:Date
 
 
 });
+Userschema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+    next();
+});
+
+//below we are creating instance method that will compare the passoword provided by user in login with the already stored passoword in database.
+//instace method is the method that can be used by all the object or document of the User collection..
+Userschema.methods.comparePassword=async function (pass,passindb){
+    return await bcrypt.compare(pass,passindb);
+
+}
+Userschema.methods.isChangedPassword=async function (tokenTIME){
+    if(!passwordChangedAt){
+        return true;
+    }
+    //here you have to convert the date at which passwordword was change to time and convert it to second
+    const passwordChangedTime=parseInt(this.passwordChangedAt.getTime()/1000,10);
+    return passwordChangedTime>tokenTIME;
+}
+//method to create reset token
+
+Userschema.methods.createResetPasswordToken=function(){
+    const resetToken=crypto.randomBytes(32).toString('hex');
+//we cannot just store this token we have to encrypt this token...
+    this.passwordResetToken=crypto.createHash('sha256').update(resetToken).digest('hex') // name of algorithm,
+    this.passwordResetTokenExpires=Date.now()+10*60*100//converted to milisecond...
+    console.log(resetToken);
+    return resetToken;
+}
+ 
 const User = new mongoose.model('User',Userschema);
 module.exports=User;
